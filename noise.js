bf0dfbe02fa98064eed4d1bd3294dea42b82c19b
Chantelle Belic <chantelle.belic@gmail.com>
2018-11-19 17:04:00 -0800

    works with key press

--- noise.js
+++ noise.js
@@ -1,12 +1,33 @@
-const playNote = () => {
-
+const clickPlay = () => {
   $('.instrument').on( 'click', 'button', function() {
 
-    const note = $(this).html();
-    document.getElementById(`${note}Audio`).play();
+    const clickNote = $(this).html();
+    // alert(`${clickNote}Audio`);
+    document.getElementById(`${clickNote}Audio`).play();
+
+    // return `${clickNote}Audio` and call this in $(document).ready
+  });
+}
+
+const typePlay = () => {
+
+  const notes = ["c", "d", "e", "f", "g", "a", "b"];
+
+  $('body').keydown(function(event) {
+    const eventNote = event.key;
+
+    if(!notes.includes(eventNote)) {
+      alert(`This note: ${eventNote} is not valid.`)
+    }
+    
+    document.getElementById(`${eventNote}Audio`).play();
+    // alert(`${eventNote}Audio`);
+    // return `${eventNote}Audio` and call this in $(document).ready
   });
 }
 
 $(document).ready( function() {
-  playNote();
+
+  clickPlay();
+  typePlay();
 });
