0d34f3608af8fd8faa247a6838d94aa0b2bacdd3
Chantelle Belic <chantelle.belic@gmail.com>
2018-11-19 16:21:07 -0800

    music works

--- noise.js
+++ noise.js
@@ -1,3 +1,12 @@
+const playNote = () => {
+
+  $('.instrument').on( 'click', 'button', function() {
+
+    const note = $(this).html();
+    document.getElementById(`${note}Audio`).play();
+  });
+}
+
 $(document).ready( function() {
-  // your code here
+  playNote();
 });
